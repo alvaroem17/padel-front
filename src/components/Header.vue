de78456d66fb44a81b471d208a26adca424c5662
Alvaro Estevez Muñoz <alvaro5estevez@gmail.com>
2026-05-11 15:07:51 +0100

    Fix header padding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-20 bg-(--primary-black) text-white flex items-center justify-between px-4">
+  <div class="w-full h-20 bg-(--primary-black) text-white flex items-center justify-between px-8">
     <div class="flex items-center space-x-2">
       <section class="w-8 h-8 bg-(--primary-yellow) text-(--primary-black) text-center leading-8 font-bold">VP</section>
       <p class="text-xl font-black tracking-tighter">VOLT<span class="text-(--primary-yellow)">PADEL</span></p>
