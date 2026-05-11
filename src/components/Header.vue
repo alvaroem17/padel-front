eb8d45d538de0b19f009b47d67a1b7275e6ad8d4
Alvaro Estevez Muñoz <alvaro5estevez@gmail.com>
2026-05-11 15:06:23 +0100

    Fix tsconfig and Main header done

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full h-20 bg-(--primary-black) text-white flex items-center justify-between px-4">
+    <div class="flex items-center space-x-2">
+      <section class="w-8 h-8 bg-(--primary-yellow) text-(--primary-black) text-center leading-8 font-bold">VP</section>
+      <p class="text-xl font-black tracking-tighter">VOLT<span class="text-(--primary-yellow)">PADEL</span></p>
+    </div>
+    <nav class="space-x-4">
+      <a href="/" class="hover:text-(--primary-yellow)">INICIO</a>
+      <a href="/canchas" class="hover:text-(--primary-yellow)">CANCHAS</a>
+      <a href="/reservas" class="hover:text-(--primary-yellow)">RESERVAR</a>
+    </nav>
+    <div>
+      <Button class="mr-2 bg-(--primary-black) hover:bg-(--primary-black) hover:text-(--primary-yellow)">
+        Iniciar Sesión
+      </Button>
+      <Button class="rounded-none bg-(--primary-yellow) text-(--primary-black) hover:bg-(--primary-yellow)/90 font-bold">
+        Registrarse
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from './ui/button/Button.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/LandingView.vue
+++ src/pages/LandingView.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+    <Header />
     <h1>Bienvenido a la página de inicio</h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import Header from '../components/Header.vue';
+
 
 </script>
 
